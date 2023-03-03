37e974817214bdddeb1cd1f9caaae2ecdb74c935
Ventsislav Iliev <ventsislav.iliew@gmail.com>
2023-03-04 00:06:33 +0200

    Exercise

---
+++ Java/Java_Basics/ForLoops_Lab/ForLoops_Exercise/MoreExercises/ClockPartTwoNesedLoops.java
@@ -0,0 +1,13 @@
+package ForLoops_Lab.ForLoops_Exercise.MoreExercises;
+
+public class ClockPartTwoNesedLoops {
+    public static void main(String[] args) {
+        for (int h = 0; h <= 23; h++) {
+            for (int m = 0; m <= 59; m++) {
+                for (int s = 0; s <=59; s++) {
+                    System.out.printf("%d : %d : %d\n",h ,m, s);
+                }
+            }
+        }
+    }
+}
